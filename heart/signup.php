11e77f48398f177f9c2f0e35a6aeaa4a8956ea2b
Никуська <138680617+kittiarabka@users.noreply.github.com>
2025-07-26 16:50:33 +0700

    Update signup.php
    исправление ошибок: теперь пользователь не сможет зарегестрироваться с одним email или телефоном больше 1 раза

--- heart/signup.php
+++ heart/signup.php
@@ -1,26 +1,45 @@
 <?php 
-    session_start();
-    require_once 'connect.php';
+session_start();
+require_once 'connect.php';
 
-    $name = $_POST['name'];
-    $number = $_POST['number'];
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-    $password_confirm = $_POST['password_confirm'];
+$name = $_POST['name'];
+$number = $_POST['number'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+$password_confirm = $_POST['password_confirm'];
 
-    if ($password === $password_confirm) {
+if ($password !== $password_confirm) {
+    $_SESSION['message'] = 'Пароли не совпадают';
+    header('Location: ../registration.php');
+    exit();
+}
 
-        mysqli_query($connect, "INSERT INTO `users` (`id`, `name`, `number`, `email`, `password`) VALUES (NULL, '$name', '$number', '$email', '$password')");
-       
+$result_email = mysqli_query($connect, "SELECT * FROM `users` WHERE `email`='$email'");
+if (mysqli_num_rows($result_email) > 0) {
+    $_SESSION['message'] = 'Этот email уже зарегистрирован';
+    header('Location: ../registration.php');
+    exit();
+}
 
-        $_SESSION['message'] = 'Регистрация прошла успешно';
-        header('Location: ../login.php');
-    } 
-    
-    else {
-        $_SESSION['message'] = 'Пароли не совпадают';
-        header('Location: ../registration.php');
-    }
+$result_number = mysqli_query($connect, "SELECT * FROM `users` WHERE `number`='$number'");
+if (mysqli_num_rows($result_number) > 0) {
+    $_SESSION['message'] = 'Этот номер телефона уже зарегистрирован';
+    header('Location: ../registration.php');
+    exit();
+}
 
+$result_name = mysqli_query($connect, "SELECT * FROM `users` WHERE `name`='$name'");
+if (mysqli_num_rows($result_name) > 0) {
+    $_SESSION['message'] = 'Это имя уже занято';
+    header('Location: ../registration.php');
+    exit();
+}
 
+$hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+mysqli_query($connect, "INSERT INTO `users` (`id`, `name`, `number`, `email`, `password`) VALUES (NULL, '$name', '$number', '$email', '$hashed_password')");
+
+$_SESSION['message'] = 'Регистрация прошла успешно';
+header('Location: ../login.php');
+exit();
 ?>
